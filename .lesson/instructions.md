4ecb93bb77d04c220814d398e94ff31b99c1baf3
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-17 07:39:42 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,17 @@
+# Instructions  
+
+Write a program that asks the user to enter five test scores. The program should display a letter grade for each score and the average test score. Write the following methods in the program:
+
+
+—This method should accept five test scores as arguments and return the average of the scores.
+
+
+—This method should accept a test score as an argument and return a letter grade for the score, based on the following grading scale:
+
+
+Score - Letter Grade
+90–100|A
+80–89|B
+70–79|C
+60–69|D
+Below 60|F
---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+   This program demonstrates a solution to the
+   Test Average and Grade programming challenge.
+*/
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+ 
+   }
+   
+   /**
+      The calcAverage method calculates the average of five
+      test scores.
+      @param score1 The first test score.
+      @param score2 The second test score.
+      @param score3 The third test score.
+      @param score4 The fourth test score.
+      @param score5 The fifth test score.
+      @return The average of the five scores.
+   */
+   
+   public static double calcAverage(double score1, double score2,
+                                    double score3, double score4,
+                                    double score5)
+   {
+   
+   }
+   
+   /**
+      The determineGrade method determines the letter grade
+      for a numeric test score.
+      @param score The numeric test score.
+      @return The letter grade.
+   */
+   
+   public static char determineGrade(double score)
+   {
+   
+   }
+}
